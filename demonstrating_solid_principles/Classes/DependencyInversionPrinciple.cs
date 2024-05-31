226e2438c58da6c178131e9e252bf47c17d43d9f
rijan999 <rijanmaharjan111@gmail.com>
2024-05-31 14:44:23 +0545

    Added SOLID Classes

---
+++ demonstrating_solid_principles/Classes/DependencyInversionPrinciple.cs
@@ -0,0 +1,48 @@
+﻿namespace demonstrating_solid_principles.Classes
+{
+    public interface IAuthWithFacebook
+    {
+        public bool VerifyFacebookAcc(string facebookId, string email, string password);
+    }
+    
+    public interface IAuthWithEmail
+    {
+        public bool VerifyAcc(string email, string password);
+    }
+
+    public class AuthWithFacebook : IAuthWithFacebook
+    {
+        public bool VerifyFacebookAcc(string facebookId, string email, string password)
+        {
+            var listOfSocialMediaUsername = new string[] { "jon", "dawg", "gaurav", "joerogan" };
+            if (facebookId is not null && listOfSocialMediaUsername.Contains(facebookId) && password.Length > 8)
+                return true;
+
+            return false;
+        }
+    }
+
+
+    public class AuthWithEmail : IAuthWithEmail
+    {
+        public bool VerifyAcc(string email, string password)
+        {
+            var listOfEmail = new string[] { "jon@gmail.com", "dawg@gmail.com", "gaurav@gmail.com", "joerogan@gmail.com" };
+            if (email is not null && listOfEmail.Contains(email) && password.Length > 8)
+                return true;
+
+            return false;
+        }
+    }
+
+
+    public class CallingCode(IAuthWithFacebook _authWithFacebook)
+    {
+        private readonly IAuthWithFacebook authWithFacebook = _authWithFacebook;
+
+        public bool VerifyAuth(string userName, string email, string password)
+        {
+            return _authWithFacebook.VerifyFacebookAcc(userName, email, password);
+        }
+    }
+}
---
+++ demonstrating_solid_principles/Classes/OpenClosePrinciple.cs
@@ -0,0 +1,105 @@
+﻿using demonstrating_solid_principles.Classes.EmailAndOther;
+using demonstrating_solid_principles.Classes.GoodExample;
+using demonstrating_solid_principles.Classes.ViewModels;
+
+namespace demonstrating_solid_principles.Classes.OpenClosePrinciple
+{
+
+    /*
+     Open/Closed Principle (OCP)
+     Principle: Software entities should be open for extension but closed for modification.
+     
+     Here Authenticate class is open for extension ,
+     but cannot be modified due to abstract property
+     */
+    public abstract class Authenticate
+    {
+        private string[] Users = ["ram", "sam", "john", "tom", "hari"];
+        private string[] Emails = ["ram@gmail.com", "sam@gmail.com", "john@gmail.com", "tom@gmail.com", "hari@gmail.com"];
+
+        public bool VerifyUser(UserInputs _inputs)
+        {
+            /* when everthing goes fine */
+            if (ValidateUser(_inputs) && RegisterUser(_inputs))
+            {
+                string toEmail = _inputs.Email;
+                string subject = "New email from Auth Module";
+                string body = $"Account has been created successfully. \n" +
+                                $"Username: {_inputs.UserName} \n" +
+                                $"Email: {_inputs.Email} \n" +
+                                $"Password: {_inputs.Password} \n";
+                
+                EmailHelper email = new EmailHelper();
+                email.SendEmail(toEmail, subject, body);
+                return true;
+            }
+
+            /* when something goes wrong */
+            return false;
+        }
+
+        /// <summary>
+        /// Function to validate user inputs.
+        /// </summary>
+        /// <param name="_inputs"></param>
+        /// <returns></returns>
+        private bool ValidateUser(UserInputs _inputs)
+        {
+            if (string.IsNullOrWhiteSpace(_inputs.UserName) || string.IsNullOrWhiteSpace(_inputs.Email) || string.IsNullOrWhiteSpace(_inputs.Password))
+            {
+                Console.WriteLine("Username, Email and Password are required field.");
+                return false;
+            }
+
+            if (Emails.Contains(_inputs.Email))
+            {
+                Console.WriteLine("Email already exists. Please sign in.");
+                return false;
+            }
+
+            if (Users.Contains(_inputs.UserName))
+            {
+                Console.WriteLine("User name already exists.");
+                return false;
+            }
+
+            if (_inputs.Password.Length < 8)
+            {
+                Console.WriteLine("Password length must be atleast 8 characters.");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Function to register user.
+        /// </summary>
+        /// <param name="_inputs"></param>
+        /// <returns></returns>
+        private bool RegisterUser(UserInputs _inputs)
+        {
+            Console.WriteLine($"New user {_inputs.UserName} has been created.");
+            return true;
+        }
+    }
+
+    public class Auth : Authenticate
+    {
+        /// <summary>
+        /// For this auth module, i will add a function to check if the user was a past user.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPastUser(UserInputs _inputs)
+        {
+            var pastUserList = new List<string>()
+            {
+                "volk@gmail.com", "chael@gmail.com", "topuria@gmail.com", "islam@gmail.com", "gauravmaharjan@gmail.com"
+            };
+
+            if (pastUserList.Contains(_inputs.Email))
+                return true;
+
+            return false;
+        }
+    }
+}
